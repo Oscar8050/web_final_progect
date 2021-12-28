9980ad8762b620ae1750cfaa4c99fc98309ab791
pei9733 <10040905paron@gmail.com>
2021-12-28 11:40:05 +0800

    \46

--- hack3/frontend/src/graphql/mutations.js
+++ hack3/frontend/src/graphql/mutations.js
@@ -12,49 +12,21 @@ export const UPDATE_TASK_MUTATION = gql`
   }
 `;
 
-// TODO 4.1 Create Task Mutation.
-// Uncomment the following lines and fill the gql part
-// export const CREATE_TASK_MUTATION = gql`
-// mutation createTask(
-//   $id: ID!
-//   $title: String!
-//   $content: String!
-//   $dueDate: Date!
-//   $status: Status!) {
-//     createTask(
-//     input:{
-//       id: $id
-//       title: $title
-//       content: $content  
-//       dueDate: $dueDate
-//       status: $status
-//     }
-//   ) {
-//       id
-//       title
-//       content
-//       dueDate
-//       status
-//     }
-//   }
-// `;
 export const CREATE_TASK_MUTATION = gql`
-  mutation createPost(
-    $id: ID!
+mutation createTask(
+  $id: ID!
   $title: String!
   $content: String!
   $dueDate: Date!
-  $status: Status!
-  ) {
+  $status: Status!) {
     createTask(
-      input:{
+      input: {
       id: $id
       title: $title
-      content: $content  
+      content: $content
       dueDate: $dueDate
       status: $status
-    }
-    ) {
+    }) {
       id
       title
       content
